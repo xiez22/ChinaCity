901c51759ce190f3957b4458be2ab4d055ef848b
ligongzzz <xiezhe20001128@163.com>
2018-03-09 18:43:43 +0800

    icon

--- app/build/gen/com/China/ChinaCity/R.java
+++ app/build/gen/com/China/ChinaCity/R.java
@@ -19,12 +19,14 @@ public final class R {
         public static final int grey_corner_view=0x7f020001;
         public static final int ic_launcher=0x7f020002;
         public static final int icon_back=0x7f020003;
-        public static final int image_icon=0x7f020004;
-        public static final int image_title=0x7f020005;
-        public static final int list_corner_view=0x7f020006;
-        public static final int none_corner_view=0x7f020007;
-        public static final int purple_corner_view=0x7f020008;
-        public static final int white_corner_view=0x7f020009;
+        public static final int icon_close=0x7f020004;
+        public static final int icon_share=0x7f020005;
+        public static final int image_icon=0x7f020006;
+        public static final int image_title=0x7f020007;
+        public static final int list_corner_view=0x7f020008;
+        public static final int none_corner_view=0x7f020009;
+        public static final int purple_corner_view=0x7f02000a;
+        public static final int white_corner_view=0x7f02000b;
     }
     public static final class id {
         public static final int hiButton1=0x7f070003;
